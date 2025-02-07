415a4ed02fac02d40d80bab3e85d28f7ea03354e
julio cesar <vcjuliocesar@outlook.com>
2025-02-07 16:12:57 -0600

    add validations

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,5 @@
+import re
+from os import error
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -10,49 +12,85 @@ from .serializer import UserSerializer
 # Create your views here.
 
 
+def validate_password(password):
+    pattern = r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&*()-+=])[A-Za-z\d@#$%^&*()-+=]{8,}$"
+    return bool(re.match(pattern, password))
+
+
 @api_view(["POST"])
 def register(request):
 
-    serializer = UserSerializer(data=request.data)
+    try:
+        if not validate_password(request.data["password"]):
+            return Response(
+                {
+                    "error": "Invalid password",
+                    "message": "The password must meet the following requirements:\n"
+                    "- At least 8 characters in length\n"
+                    "- At least one uppercase letter\n"
+                    "- At least one lowercase letter\n"
+                    "- At least one number\n"
+                    "- At least one special character (@, #, $, %, &, *, etc.)",
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
-    if not serializer.is_valid():
+        serializer = UserSerializer(data=request.data)
+
+        if not serializer.is_valid():
+
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer.save()
+
+        user = User.objects.get(username=request.data["username"])
+        user.set_password(serializer.data["password"])
+        user.save()
+
+        token = Token.objects.create(user=user)
 
         return Response(
-            {"error", serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+            {"token": token.key, "user": serializer.data},
+            status=status.HTTP_201_CREATED,
         )
-
-    serializer.save()
-
-    user = User.objects.get(username=request.data["username"])
-    user.set_password(serializer.data["password"])
-    user.save()
-
-    token = Token.objects.create(user=user)
-
-    return Response(
-        {"token": token.key, "user": serializer.data}, status=status.HTTP_201_CREATED
-    )
+    except Exception as error:
+        raise error
 
 
 @api_view(["POST"])
 def login(request):
 
-    username = request.data["username"].strip()
+    try:
 
-    password = request.data["password"].strip()
+        username = request.data["username"].strip()
 
-    user = get_object_or_404(User, username=username)
+        password = request.data["password"].strip()
 
-    if not user.check_password(password):
+        if username is None or not username:
+            return Response(
+                {"error", "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        if password is None or not password:
+            return Response(
+                {"error", "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        user = get_object_or_404(User, username=username)
+
+        if not user.check_password(password):
+
+            return Response(
+                {"error", "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        token, created = Token.objects.get_or_create(user=user)
+
+        serializer = UserSerializer(instance=user)
 
         return Response(
-            {"error", "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED
+            {"token": token.key, "user": serializer.data}, status=status.HTTP_200_OK
         )
+    except Exception as error:
 
-    token, created = Token.objects.get_or_create(user=user)
-
-    serializer = UserSerializer(instace=user)
-
-    return Response(
-        {"token": token.key, "user": serializer.data}, status=status.HTTP_200_OK
-    )
+        raise error
