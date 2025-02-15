b5b4795b55303757f1d013f9ee2d667b89f651bc
julio cesar <vcjuliocesar@outlook.com>
2025-02-14 19:20:52 -0600

    add swagger config

--- notes_app/urls.py
+++ notes_app/urls.py
@@ -14,11 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from django.contrib import admin
 from django.urls import path,include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/',include('authentication.urls')),
-    path('api/v1/',include('notes.urls'))
+    path('api/v1/',include('notes.urls')),
+    #SWAGGER
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/v1/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
